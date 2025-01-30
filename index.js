4aa06cd634df473c8504c5fc60b4d9d699780923
ivan-puhachov <148248085+ivan-puhachov@users.noreply.github.com>
2025-01-30 20:40:50 +0200

    Fixed Db

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ app.use(cors());
 
 // Database configuration
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: "34.88.151.159", 
+    port: 3306, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
 });
 
